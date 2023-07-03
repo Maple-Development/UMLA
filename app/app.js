683409931cb5649e46868deb81b5cf16c513fc48
Logan <thecrazykid2121@gmail.com>
2023-07-02 21:48:30 -0400

    Updates

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'umla/config/environment';
+import NavBar from '../umla/components/nav-bar';
+import ControlBar from '../umla/components/control-bar';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
