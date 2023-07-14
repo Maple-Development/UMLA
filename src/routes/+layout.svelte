726e71bf1c1fc2d81152e3fbaba668e2f6ed7a73
Logan <thecrazykid2121@gmail.com>
2023-07-14 00:17:21 -0400

    port to svelte done

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    let title="UMLA";
+    import NavBar from "../components/nav-bar.svelte";
+    import ControlBar from "../components/control-bar.svelte";
+    import "./css/app.css";
+</script>
+<head>
+  <title>{title}</title>
+</head>
+
+<NavBar />
+<slot></slot>
+<ControlBar />
+
