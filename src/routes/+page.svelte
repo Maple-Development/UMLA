a2b6a5d6e775eb3cad01d09fb5f4bf23016bb078
Logan <thecrazykid2121@gmail.com>
2023-07-25 18:44:02 -0400

    Redirect to /upload if user likely hasn't uploaded
    Otherwise redirect to library

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,18 @@
 <script>
      import { browser } from '$app/environment';
+     import { get, set } from 'idb-keyval';
 
+     async function decideWhereToGo() {
+        const fileHandleOrUndefined = await get('file');
+            if (fileHandleOrUndefined) {
+                window.location.href = '/library';
+            }
+            else {
+                window.location.href = '/upload';
+                }
+        }
 
 if (browser) { 
-    window.location.href = '/library';
+    decideWhereToGo();
 }
 </script>
