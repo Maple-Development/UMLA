0c5b95689b1397e9c8f405ce9e67ef989e69010c
Logan <thecrazykid2121@gmail.com>
2023-07-25 19:41:33 -0400

    3mb tailwind bundle and postcss

--- src/components/nav-bar.svelte
+++ src/components/nav-bar.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { Tooltip, Button } from 'flowbite-svelte';
+  let type = 'dark';
   import { onMount } from 'svelte';
   onMount(() => {
     let hovered = false;
@@ -11,7 +13,7 @@
        const rect = e.target.getBoundingClientRect();
    
        document.getElementById("nav-move").style.width = rect.width - -45 + "px";
-       document.getElementById("nav-move").style.height = rect.height + 4 + "px";
+       document.getElementById("nav-move").style.height = rect.height + 6 + "px";
    
        requestAnimationFrame(() => {
          document.getElementById("nav-move").style.display = "block";
@@ -116,10 +118,22 @@
       </div>
       <a href="/albums" class="nav_text nav-btn">Albums</a>
     </div>
- 
+    
     <div class="profile">
+      <div id="type-dark" class="refresh_icon">
+        <svg fill="#81F0FF" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+     viewBox="0 0 489.533 489.533" xml:space="preserve">
+        <g>
+          <path fill="#81F0FF" d="M268.175,488.161c98.2-11,176.9-89.5,188.1-187.7c14.7-128.4-85.1-237.7-210.2-239.1v-57.6c0-3.2-4-4.9-6.7-2.9
+            l-118.6,87.1c-2,1.5-2,4.4,0,5.9l118.6,87.1c2.7,2,6.7,0.2,6.7-2.9v-57.5c87.9,1.4,158.3,76.2,152.3,165.6
+            c-5.1,76.9-67.8,139.3-144.7,144.2c-81.5,5.2-150.8-53-163.2-130c-2.3-14.3-14.8-24.7-29.2-24.7c-17.9,0-31.9,15.9-29.1,33.6
+            C49.575,418.961,150.875,501.261,268.175,488.161z"/>
+        </g>
+        </svg>
+      </div>
       <img src="/assets/pfp-placeholder.png" alt="Profile">
     </div>
     <div id="nav-move"></div>
+    <Tooltip {type} triggeredBy="[id^='type-']" on:show={ev => type = ev.target.id.split('-')[1]}>Refresh Library</Tooltip>
  </div>
  
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,17 @@
 <script>
-    let title="UMLA";
-    import NavBar from "../components/nav-bar.svelte";
-    import ControlBar from "../components/control-bar.svelte";
-    import "./css/app.css";
-    import "./css/bottom-bar.css";
+  import "../app.postcss";
+  let title = "UMLA";
+  import NavBar from "../components/nav-bar.svelte";
+  import ControlBar from "../components/control-bar.svelte";
+  import "./css/app.css";
+  import "./css/bottom-bar.css";
 </script>
+
 <head>
   <title>{title}</title>
 </head>
 
 <NavBar />
-<slot></slot>
-<br><br><br><br>
+<slot />
+<br /><br /><br /><br />
 <ControlBar />
