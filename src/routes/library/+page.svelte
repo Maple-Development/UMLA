bffb7a3e793db3e9b41790aa4510503f48b63095
Logan <Cattn@mathstudy.dev>
2023-09-27 12:08:33 -0400

    feat: material theming

--- src/routes/library/+page.svelte
+++ src/routes/library/+page.svelte
@@ -3,6 +3,8 @@
   import { songs } from '$lib/store.js';
 </script>
 
+<div class="favorites">
+  <h1 class="favorites_header">Favorite Playlists</h1>
 <div class="outer-card">
   {#if $songs && $songs.length > 0}
     {#each $songs as song}
@@ -17,3 +19,21 @@
     {/each}
   {/if}
 </div>
+</div>
+
+<!--
+<div class="outer-card">
+  {#if $songs && $songs.length > 0}
+    {#each $songs as song}
+      <LargeSongCard
+        fileName={song.fileName}
+        title={song.title}
+        artist={song.artist}
+        albumArt={song.albumArt}
+        type="song"
+        id={song.id}
+      />
+    {/each}
+  {/if}
+</div>
+-->
