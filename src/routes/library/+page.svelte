c67b5b157dc85d5a4eb551bba8880d60ff559c76
Logan <Cattn@mathstudy.dev>
2023-09-27 17:46:48 -0400

    feat: tweak new ui

--- src/routes/library/+page.svelte
+++ src/routes/library/+page.svelte
@@ -1,12 +1,31 @@
 <script>
   import LargeSongCard from '../../components/large-song-card.svelte';
-  import { songs } from '$lib/store.js';
+  import { songs, playlists } from '$lib/store.js';
 </script>
 
+{#if $songs && $songs.length > 0}
+{#if $playlists.length > 0}
+    <div class="favorites">
+      <h1 class="favorites_header">Favorite Playlists</h1>
+    <div class="outer-card">
+        {#each $playlists as playlist}
+          <LargeSongCard
+            fileName=""
+            title={playlist.title}
+            artist=""
+            albumArt={playlist.playlistArt}
+            type="playlist"
+          />
+        {/each}
+      </div>
+    </div>
+{/if}
+{/if}
+
+{#if $songs && $songs.length > 0}
 <div class="favorites">
-  <h1 class="favorites_header">Favorite Playlists</h1>
+  <h1 class="favorites_header">Recently Added</h1>
 <div class="outer-card">
-  {#if $songs && $songs.length > 0}
     {#each $songs as song}
       <LargeSongCard
         fileName={song.fileName}
@@ -17,9 +36,9 @@
         id={song.id}
       />
     {/each}
-  {/if}
 </div>
 </div>
+{/if}
 
 <!--
 <div class="outer-card">
